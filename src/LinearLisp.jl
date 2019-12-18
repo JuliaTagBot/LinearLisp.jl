a2f1840b2888ff442f224c231b9901f74d305af3
Carl Morris <carl.morris.world@outlook.com>
2019-12-19 07:37:08 +0800

    Add basic scope based approach that handles def

--- src/LinearLisp.jl
+++ src/LinearLisp.jl
@@ -2,6 +2,139 @@ module LinearLisp
 
 using Tokenize
 
+abstract type Binding end
+
+struct ValueBinding <: Binding
+    name
+    value  # Julia literal
+end
+
+value(binding::ValueBinding) = binding.value
+
+struct FunctionBinding <: Binding
+    name
+    params
+    scope
+    arity
+    body  # tokens
+end
+
+params(binding::FunctionBinding) = binding.params
+scope(binding::FunctionBinding) = binding.scope
+arity(binding::FunctionBinding) = binding.arity
+body(binding::FunctionBinding) = binding.body
+
+function FunctionBinding(name, parent_scope, expr)
+    FunctionBinding(
+        name, params, empty_scope(parent_scope), expr.arity, expr.body)
+end
+
+struct FunctionExpr
+    params
+    arity
+    body
+end
+
+struct SyntaxBinding <: Binding
+    name
+    form  # tokens
+end
+
+struct PrimitiveBinding <: Binding
+    name
+    handler  # Julia function
+end
+
+handler(binding::PrimitiveBinding) = binding.handler
+
+struct Scope
+    bindings  # mapping Symbol to Binding
+    parent  # nothing or next scope to search for binding.
+end
+
+name(binding::Binding) = binding.name
+
+function bind!(scope::Scope, binding_name, value)
+    # @show binding_name value
+    if value isa FunctionExpr
+        binding = FunctionBinding(binding_name, scope, value)
+    elseif value isa Binding
+        binding = value
+    else
+        binding = ValueBinding(binding_name, value)
+    end
+    setindex!(scope.bindings, value, binding_name)
+    scope
+end
+function get(scope::Scope, name::Symbol)
+    scope.bindings[name]
+end
+function get(scope::Scope, name::Union{Number,String})
+    name
+end
+
+empty_scope(parent=nothing) = load_primitives!(Scope(
+    Dict{Symbol,Any}(),
+    parent,
+))
+
+function load_primitives!(scope)
+    bind!(scope, :def, PrimitiveBinding(:def, handle_def!))
+    bind!(scope, :fn, PrimitiveBinding(:fn, handle_fn!))
+    bind!(scope, :syntax, PrimitiveBinding(:syntax, handle_syntax!))
+end
+
+function handle_def!(scope, L)
+    # expected L = [:def, name, value, ...]
+    @show scope L
+    name = L[2]
+    restL = @view L[3:end]
+    @show restL
+    value, newL = eval_binding(scope, restL)
+    bind!(scope, name, value), newL
+end
+
+function handle_fn!(scope, L)
+    @show scope L
+end
+
+function handle_syntax!(scope, L)
+    @show scope L
+end
+
+function eval_binding(scope, L)
+    @show scope L
+    binding = get(scope, L[1])
+    @show binding
+    if binding isa PrimitiveBinding
+        @show "PrimitiveBinding"
+        binding_value, newL = handler(binding)(scope, L)
+    elseif binding isa FunctionBinding
+        @show "FunctionBinding"
+        args, newL = eval_function_args(scope, arity(binding), @view L[2:end])
+        fn_scope = scope(binding)
+        for (p, a) in zip(params(binding), args)
+            bind!(fn_scope, p, a)
+        end
+        binding_value, _ = eval_binding(fn_scope, body(binding))
+    elseif binding isa SyntaxBinding
+        @show "SyntaxBinding"
+        L = binding(scope, L)
+        binding_value, newL = eval_binding(scope, L)
+    elseif binding isa ValueBinding
+        @show "ValueBinding"
+        binding_value, newL = value(binding), @view L[2:end]
+    else  # literal value
+        @show "Literal"
+        binding_value, newL = binding, @view L[2:end]
+    end
+    binding_value, newL
+end
+
+function eval_function_args(scope, arity, L)
+    @show scope arity L
+end
+
 const IDENTIFIER = Tokenize.Tokens.IDENTIFIER
 const WHITESPACE = Tokenize.Tokens.WHITESPACE
 const STRING = Tokenize.Tokens.STRING
@@ -36,171 +169,6 @@ function make_lexpr(source)
         end
     end
     expr
-end
-
-function eval_lexpr(L)
-    null_lexpr(L) && return L
-    atom = L[1]
-    if atom === DEF_ID
-        if length(L) < 3
-            println("Invalid def expression at")
-            println(t)
-            return []
-        end
-        let target = L[2], value = L[3]
-            @eval Primitives begin
-                $(target) = $(value)
-            end
-        end
-        L[4:end]
-    elseif atom === DEFUN_ID
-        close_i = findnext(x -> x == LL.DEFUN_END_ID, L, 1)
-        let name = L[2], param = L[3], body = L[4:close_i-1]
-            @eval Primitives begin
-                function $(name)($(param))
-                    @show $(body)
-                    $(program)($(body), true)
-                end
-            end
-        end
-        return L[close_i+1:end]
-    else
-        value = Base.eval(Primitives, atom)
-        if value isa String
-            return [value; L[2:end]]
-        else
-            # @show value L[2:end]
-            return value(L[2:end])
-        end
-    end
-end
-
-function program(L, quiet=False)
-    while !null_lexpr(L)
-        # @show L
-        L = let new_L = eval_lexpr(L)
-            # @show new_L
-            if length(new_L) == length(L)
-                println(new_L[1])
-                new_L = new_L[2:end]
-            end
-            new_L
-        end
-    end
-end
-
-# recursively expand tokens into Julia expressions for evaluation.
-function expand_all(L)
-    L = copy(L)
-    expanded_expr = []
-    while length(L) > 0
-        @show L expanded_expr
-        expr, L = expand(L)
-        push!(expanded_expr, expr)
-        # DEBUG infinit loop
-        # if length(expanded_expr) > 4
-        #     break
-        # end
-    end
-    expanded_expr
-end
-
-function expand(L)
-    token = L[1]
-    if token isa String
-        expr = Meta.parse(L[1])
-        L = L[2:end]
-    elseif token in keys(SYNTAX)
-        handler = SYNTAX[token]
-        expr, L = handler(L)
-    else
-        # TODO: expand function calls.
-        @show token "Unhandled function call or variable lookup."
-        expr, L = L[1], L[2:end]
-    end
-    expr, L
-end
-
-# primitive forms
-#
-# We should first expand the tokens into primitive forms and then emit code.
-# This allows us to track important information like function arity that's
-# not convenient to track in Julia.
-struct DefExpr
-    target
-    value # Expr
-    code
-end
-
-struct LambdaExpr
-    params
-    body # Expr
-    code
-end
-
-arity(expr::LambdaExpr) = length(expr.params)
-
-# primitive syntax
-#
-# converts L expressions into Julia expressions.
-function emit_def(L)
-    # L[1] is keyword def, we skip
-    target = L[2]
-    value, L = expand(L[3:end])
-    code = :($(target) = $(value))
-    DefExpr(target, value, code), L
-end
-
-function emit_lambda_named_args(L)
-    # L[1] is keyword lambda, we skip.
-    # until do determines what if any parameters the lambda has.
-    i = findnext(x -> x === LAMBDA_BODY_START_ID, L, 1)
-    params = L[2:i-1]
-    j = findnext(x -> x === LAMBDA_BODY_END_ID, L, i)
-    body = L[i+1:j-1]
-    # TODO: eval body for syntax and convert to primitves.
-    @show params body
-    code = Expr(:->, Expr(:tuple, params...), Expr(:block, body...))
-    return LambdaExpr(params, body, code), L[j+1:end]
-end
-
-function emit_function_call(L)
-    @show L
-end
-
-# TODO: Find better way to track arity directly.
-function arity(lambda)
-    return length(first(methods(lambda)).sig.parameters) - 1
-end
-
-
-# syntax map to determine how to emit Julia from the Token stream.
-SYNTAX = Dict{Symbol, Any}([
-    DEF_ID => emit_def,
-    LAMBDA_ID => emit_lambda_named_args
-])
-
-# Store arity of functions to enable parsing.
-# TODO: Avoid a global symbol table.
-FUNCTIONS = Dict{Symbol, Int}()
-
-
-baremodule Primitives
-
-import Base
-import ..LL
-
-function print(L)
-    Base.println("My print")
-    L = eval(L)
-    Base.println(L[1])
-    L[2:end]
-end
-
-function eval(L)
-    LL.eval_lexpr(L)
-end
-
 end
 
 end # module
